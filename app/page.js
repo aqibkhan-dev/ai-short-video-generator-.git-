832f4699c30d8a69d8e5502bd3c08dadc405098d
unknown <https://github.com/aqibkhan-dev/>
2025-01-07 15:57:16 +0530

    ai first project

--- app/page.js
+++ app/page.js
@@ -1,17 +1,12 @@
-import React from "react";
-import { UserButton } from "@clerk/nextjs";
-import { Button } from "../components/ui/button";
-import Header from "./dashboard/_components/Header";
-import SideNav from "./dashboard/_components/SideNav";
+import React from 'react'
+import Header from './dashboard/_components/Header'
+import SideNav from './dashboard/_components/SideNav'
 
-
-
-
-export default function Home() {
+export default function Home () {
   return (
     <div>
-   <Header/>
-   <SideNav/>
-     </div>
-  );
+      <Header />
+      <SideNav />
+    </div>
+  )
 }
